6cb5e2738e5b2202d61c28a252e0e452805fa164
diego_loch <cout3728@gmail.com>
2026-03-28 00:31:56 -0600

    nueva forma de leer datos en java uso de Console en lugar del Scanner

---
+++ fizzbooz.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class fizzbooz {
+
+    public static void main(String[] args) {
+        Console o = System.console();
+        System.out.println("ingre");
+        int a = Integer.parseInt(o.readLine());
+
+        System.out.println(a);
+    }
+}
